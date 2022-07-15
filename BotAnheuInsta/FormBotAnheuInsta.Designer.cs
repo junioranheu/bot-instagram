7a0a1eecb5ebbc3a471e7fd5b348d0c432267048
junioranheu <junioranheu@gmail.com>
2022-07-15 11:23:31 -0300

    primeiro commit do projeto

---
+++ BotAnheuInsta/FormBotAnheuInsta.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace BotAnheuInsta
+{
+    partial class FormBotAnheuInsta
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.BtnIniciar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.lblAviso = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(8)))), ((int)(((byte)(55)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(771, 12);
+            this.panel2.TabIndex = 4;
+            // 
+            // BtnIniciar
+            // 
+            this.BtnIniciar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(182)))), ((int)(((byte)(172)))), ((int)(((byte)(0)))));
+            this.BtnIniciar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnIniciar.FlatAppearance.BorderSize = 0;
+            this.BtnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnIniciar.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnIniciar.ForeColor = System.Drawing.Color.White;
+            this.BtnIniciar.Location = new System.Drawing.Point(36, 427);
+            this.BtnIniciar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.BtnIniciar.Name = "BtnIniciar";
+            this.BtnIniciar.Size = new System.Drawing.Size(692, 33);
+            this.BtnIniciar.TabIndex = 16;
+            this.BtnIniciar.Text = "Gerar comentários";
+            this.BtnIniciar.UseVisualStyleBackColor = false;
+            this.BtnIniciar.Click += new System.EventHandler(this.BtnIniciar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(174)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(121, 119);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 32);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Anheu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Elephant", 65F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(174)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(14, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 113);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Bot";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(36, 191);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(574, 82);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Gerador automático de \r\nlikes e comentários no Instagram\r\n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(38, 283);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Versão 1.0";
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSair.Location = new System.Drawing.Point(686, 20);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(75, 23);
+            this.BtnSair.TabIndex = 17;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = true;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // lblAviso
+            // 
+            this.lblAviso.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblAviso.Location = new System.Drawing.Point(342, 283);
+            this.lblAviso.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAviso.Name = "lblAviso";
+            this.lblAviso.Size = new System.Drawing.Size(386, 126);
+            this.lblAviso.TabIndex = 18;
+            this.lblAviso.Text = "{lblAviso}";
+            this.lblAviso.Visible = false;
+            // 
+            // FormBotAnheuInsta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 481);
+            this.Controls.Add(this.lblAviso);
+            this.Controls.Add(this.BtnSair);
+            this.Controls.Add(this.BtnIniciar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FormBotAnheuInsta";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bot Anheu spammador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button BtnIniciar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnSair;
+        private System.Windows.Forms.Label lblAviso;
+    }
+}
+
---
+++ BotAnheuInsta/FormBotAnheuInsta.cs
@@ -0,0 +1,247 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BotAnheuInsta
+{
+    public partial class FormBotAnheuInsta : Form
+    {
+        // Para que o Selenium funcione é necessário instalar:
+        // 01 - Selenium.WebDriver;
+        // 02 - Selenium.WebDriver.ChromeDriver;
+
+        // Wards:
+        // Ideia baseada em: https://www.youtube.com/watch?v=nb_gbWzGpPM&feature=youtu.be&t=1184;
+        // Incognito: https://stackoverflow.com/questions/38643340/selenium-webdriver-chrome-c-sharp-unable-to-launch-selenium-browser-in-inc;
+        // Como selecionar um input, classe, etc.: https://saucelabs.com/resources/articles/selenium-tips-css-selectors;
+        // Buscar elemento com timer: https://stackoverflow.com/questions/6992993/selenium-c-sharp-webdriver-wait-until-element-is-present;
+        // Clicar em algum elemento (usando Javascript): https://stackoverflow.com/questions/35259012/click-function-on-selenium-not-working-in-c-sharp;
+
+        // Variáveis constantes;
+        const string login = "xxx", senha = "IssoEApenasUmTeste@";
+        const int qtdScrollParaBaixoBusca = 8;
+
+        public FormBotAnheuInsta()
+        {
+            InitializeComponent();
+
+            // Aviso escondido;
+            lblAviso.Visible = false;
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnIniciar_Click(object sender, EventArgs e)
+        {
+            // Inciciar variáveis;
+            lblAviso.Text = "";
+            string hashtagBuscada = "naruto";
+
+            // Iniciar conexão entre o Selenium e navegador, e abrir o Instagram;
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--incognito");
+            ChromeDriver driver = new ChromeDriver(Environment.CurrentDirectory, options);
+            driver.Navigate().GoToUrl("http://instagram.com");
+
+            // Aguardar 1 segundo para iniciar o processo;
+            Thread.Sleep(1000);
+            try
+            {
+                // PARTE 01: Login;
+                IWebElement elementoLogin = BuscarElemento(driver, By.CssSelector("input[name='username']"), 20);
+                elementoLogin.SendKeys(login);
+
+                IWebElement elementoSenha = BuscarElemento(driver, By.CssSelector("input[name='password']"), 20);
+                elementoSenha.SendKeys(senha);
+                Aviso("Nome de usuário e senha preenchidos");
+
+                IWebElement elementoBotaoLogin = BuscarElemento(driver, By.CssSelector("button[type='submit']"), 20);
+                ClicarElemento(driver, elementoBotaoLogin);
+
+                // PARTE 02: Clicar para não receber notificações;
+                Thread.Sleep(2000);
+                Aviso("Realizando login e clicando para não receber notificações...");     
+                IWebElement elementoNotificacao = BuscarElemento(driver, By.CssSelector("._a9_1"), 20);
+                ClicarElemento(driver, elementoNotificacao);
+                Thread.Sleep(1000);
+                Aviso("Botão de não receber notificações clicado...");
+
+                // PARTE 03: Busca da hashtag;
+                Thread.Sleep(5000);
+                Aviso("Login realizado com sucesso\nBuscando por #" + hashtagBuscada + "\nAguarde uns instantes...");
+                IWebElement elentoCaixaBusca = BuscarElemento(driver, By.CssSelector("._aawh"), 20);
+                elentoCaixaBusca.SendKeys(hashtagBuscada);
+                driver.Navigate().GoToUrl("http://instagram.com/explore/tags/" + hashtagBuscada + "/");
+                Aviso("Carregando resultados\nAguarde uns instantes...");
+
+                // Esperar mais alguns segundos pra carregar as imagens;
+                Thread.Sleep(3000);
+
+                // Dar scroll para baixo x vezes para carregar mais itens;
+                // Cada scroll demora mais 2 segundos
+                for (int i = 0; i < qtdScrollParaBaixoBusca; i++)
+                {
+                    Aviso("Carregando resultados\nScroll " + (i + 1).ToString() + " de " + qtdScrollParaBaixoBusca + "\nAguarde uns instantes...");
+                    ScrollMaximoPraBaixo(driver);
+                    Thread.Sleep(2000);
+                }
+
+                // Finalmente buscar e armazenar os links das fotos encontradas;
+                // Explicação dessa parte no link a seguir: https://youtu.be/nb_gbWzGpPM?t=1704;
+                IList<IWebElement> listaItensEncontrados = driver.FindElements(By.TagName("a"));
+                List<string> listaURL = new List<string>();
+                foreach (var item in listaItensEncontrados)
+                {
+                    string link = item.GetAttribute("href");
+                    listaURL.Add(link);
+                }
+
+                Aviso("Foram encontradas " + listaURL.Count + " fotos\nPreparando para curtir e comentar...");
+
+                // PARTE 03: Navegar na lista de URLs para realizar os comentários;
+                int j = 1;
+                foreach (var url in listaURL)
+                {
+                    // Ir até a foto;
+                    driver.Navigate().GoToUrl(url);
+                    Thread.Sleep(3000);
+
+                    // Checar se a foto já está curtida: https://stackoverflow.com/questions/63907587/python-selenium-how-do-i-check-if-i-liked-a-post-on-instagram-or-not;
+                    // Se a foto tiver curtida, pule a foto;
+                    var elementoChecarFotoCurtida = BuscarElemento(driver, By.CssSelector("._aamw button._abl- div._abl_ span svg"), 20);
+                    string cor = elementoChecarFotoCurtida.GetAttribute("fill");
+                    if (cor != "#ed4956")
+                    {
+                        // Curtir foto;
+                        IWebElement elementoCurtir = BuscarElemento(driver, By.CssSelector("._aamw button._abl-"), 20);
+                        ClicarElemento(driver, elementoCurtir);
+
+                        // Comentar (pro comentário funcionar, foi necessário usar duas vezes o Click(): https://stackoverflow.com/questions/53502191/send-a-instagram-comment-using-python-with-selenium/);
+                        string comentario = GerarComentario();
+
+                        try
+                        {
+                            IWebElement elementoCaixaComentario = BuscarElemento(driver, By.ClassName("_aaoc"), 3);
+                            elementoCaixaComentario.Click();
+                            elementoCaixaComentario = BuscarElemento(driver, By.ClassName("_aaoc"), 20);
+                            elementoCaixaComentario.Click();
+                            EscreverComentario(elementoCaixaComentario, comentario);
+
+                            // Enviar comentário;
+                            IWebElement elementoBotaoPublicar = BuscarElemento(driver, By.CssSelector("button[type='submit']"), 20);
+                            ClicarElemento(driver, elementoBotaoPublicar);
+                        }
+                        catch (Exception)
+                        {
+                            Aviso("Essa última foto não pode ser curtida e/ou comentada porque haviam restrições");
+                        }
+                        finally
+                        {         
+                            j++;
+                            Aviso("Foram encontradas " + listaURL.Count + " fotos\nFotos comentadas e curtidas: " + j.ToString());
+                        }
+                 
+                        Thread.Sleep(1000);
+                    }
+                }
+
+                Aviso("Processo finalizado\nForam encontradas " + listaURL.Count + " fotos\nFotos comentadas e curtidas: " + j.ToString() + "");
+                driver.Close();
+            }
+            catch (Exception ex)
+            {
+                Aviso("Houve um erro durante o processo\n-> " + ex.Message);
+            }
+        }
+
+        // Escrever comentário de forma "humana";
+        private void EscreverComentario(IWebElement elemento, string comentario)
+        {
+            foreach (char letra in comentario)
+            {
+                elemento.SendKeys(letra.ToString());
+                Thread.Sleep(100);
+            }
+        }
+
+        // Gerar comentário aleatório;
+        private string GerarComentario()
+        {
+            //var listaComentarios = new List<string> {
+            //    "Olá! Quer aprender um pouco mais sobre xxx? Me siga aqui :)",
+            //    "Foto da hora! Quer aprender um pouco mais sobre xxx? Me siga aqui :)",
+            //    "Gostei da foto! Quer aprender um pouco mais sobre xxx? Me siga aqui :)",
+            //    "Oi, né? Quer aprender um pouco mais sobre xxx? Me siga aqui :)" 
+            //};
+
+            var listaComentarios = new List<string> {
+                "Isso é apenas um #teste 01",
+                "Isso é apenas um #teste 02",
+                "Isso é apenas um #teste 03",
+                "Isso é apenas um #teste 04",
+            };
+
+            var random = new Random();
+            int i = random.Next(listaComentarios.Count);
+            string comentarioAleatorio = listaComentarios[i];
+
+            return comentarioAleatorio;
+        }
+
+        // Dar um scroll pra baixo na tela (Instagram) usando Javascript;
+        private void ScrollMaximoPraBaixo(IWebDriver driver)
+        {
+            IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+            executor.ExecuteScript("window.scrollTo(0, document.body.scrollHeight);");
+        }
+
+        // Clicar em algum elemento (usando Javascript);
+        private void ClicarElemento(IWebDriver driver, IWebElement elemento)
+        {
+            IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+            executor.ExecuteScript("arguments[0].click();", elemento);
+        }
+
+        // Buscar elemento;
+        private IWebElement BuscarElemento(IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            if (timeoutInSeconds >= 10)
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+                IWebElement elemento = wait.Until(drv => drv.FindElement(by));
+                return elemento;
+            }
+
+            return driver.FindElement(by);
+        }
+
+        private void Aviso(string aviso)
+        {
+            lblAviso.Text = aviso;
+            lblAviso.Visible = true;
+            Application.DoEvents();
+        }
+
+        // Permitir mover o programa;
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+    }
+}
